35ccdfa6c5e1c502e7582751b1f3654195f46731
F R Summit <www.frsummit1@gmail.com>
2018-02-12 17:42:46 +0600

    load specific payroll complete

--- src/main/java/com/frsummit/HRM/controller/admin/payroll/SetPayroll.java
+++ src/main/java/com/frsummit/HRM/controller/admin/payroll/SetPayroll.java
@@ -1,8 +1,14 @@
 package com.frsummit.HRM.controller.admin.payroll;
 
+import com.frsummit.HRM.model.Payroll;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 @Controller
 public class SetPayroll {
@@ -10,5 +16,13 @@ public class SetPayroll {
     @RequestMapping(value = "/admin/payroll-admin-set-payment", method = RequestMethod.GET)
     public String setUserPayrollForm(){
         return "payroll_admin_set_payment";
+    }
+
+    @RequestMapping(value = "/admin/set-payment", method = RequestMethod.POST)
+    public ModelAndView createNewUser(@Valid Payroll payroll, BindingResult bindingResult, Model model) {
+
+        ModelAndView modelAndView = new ModelAndView();
+            modelAndView.setViewName("home");
+        return modelAndView;
     }
 }
