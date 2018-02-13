7d259c7d6c403a05672d54bc45ad0cc441de245c
F R Summit <www.frsummit1@gmail.com>
2018-02-13 22:47:58 +0600

    user update controlling part 2

--- src/main/java/com/frsummit/HRM/controller/admin/profile/UpdateUser.java
+++ src/main/java/com/frsummit/HRM/controller/admin/profile/UpdateUser.java
@@ -1,6 +1,8 @@
 package com.frsummit.HRM.controller.admin.profile;
 
 import com.frsummit.HRM.model.User;
+import com.frsummit.HRM.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -14,6 +16,9 @@ import javax.validation.Valid;
 @Controller
 public class UpdateUser {
 
+    @Autowired
+    private UserService userService;
+
     @RequestMapping(value = "/admin/profile-user-update", method = RequestMethod.GET)
     public String updateUser(){
         return "profile_user_update";
@@ -21,8 +26,10 @@ public class UpdateUser {
 
     @RequestMapping(value = "/admin/profile-update", method = RequestMethod.GET)
     public ModelAndView updateUser(@Valid User user, BindingResult bindingResult, Model model,
-                                   @RequestParam(value = "firstName") String firstName){
+                                   @RequestParam(value = "firstName") String firstName,
+                                   @RequestParam(value = "userRole") String userRole){
         ModelAndView modelAndView = new ModelAndView();
+        userService.saveUser(user, userRole);
         System.out.println(firstName);
         modelAndView.setViewName("profile_user_list");
         return modelAndView;
